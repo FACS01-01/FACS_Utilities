cbb93c7db8324122c83ea1828d36e316af170b1b
FACS01-01 <75183551+FACS01-01@users.noreply.github.com>
2021-09-26 23:37:35 -0300

    Update FACSLoadBundle2019Editor.cs
    Can select AssetBundle with "open file" dialog.

--- Editor/FACSLoadBundle2019Editor.cs
+++ Editor/FACSLoadBundle2019Editor.cs
@@ -8,22 +8,33 @@ namespace FACS01.Utilities
     [CustomEditor(typeof(FACSLoadBundle2019))]
     public class FACSLoadBundle2019Editor : Editor
     {
-
         private List<string> ShaderUsageIn;
         private List<List<string>> MaterialUsageIn;
-        private Vector2 scrollPos;
+        private readonly string[] filters = { "VRChat Files (vrca, vrcw)", "vrca,vrcw", "All files", "*" };
         public override void OnInspectorGUI()
         {
             FACSLoadBundle2019 LoadBundleScript = (FACSLoadBundle2019)target;
-
-            LoadBundleScript.AssetSource = EditorGUILayout.TextField("Asset Source: ", LoadBundleScript.AssetSource);
-            LoadBundleScript.Name = EditorGUILayout.TextField("Asset Name: ", LoadBundleScript.Name);
-            EditorGUILayout.Space();
-            LoadBundleScript.ShaderUsage = EditorGUILayout.Foldout(LoadBundleScript.ShaderUsage, "Shaders used in Asset", true);
-
             GUIStyle newstyle = new GUIStyle(GUI.skin.GetStyle("HelpBox"));
             newstyle.richText = true;
             newstyle.fontSize = 12;
+            EditorGUIUtility.labelWidth = 94;
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button($"<color=green>Bundle Source</color>", newstyle, GUILayout.Height(20), GUILayout.Width(93)))
+            {
+                string path = EditorUtility.OpenFilePanelWithFilters("Select Asset Bundle", "", filters);
+                if (path.Length != 0)
+                {
+                    GUIUtility.keyboardControl = 0;
+                    LoadBundleScript.AssetSource = path;
+                }
+            }
+            LoadBundleScript.AssetSource = EditorGUILayout.TextField("", LoadBundleScript.AssetSource);
+            EditorGUILayout.EndHorizontal();
+
+            LoadBundleScript.Name = EditorGUILayout.TextField(" Bundle Name", LoadBundleScript.Name);
+
+            EditorGUILayout.Space();
+            LoadBundleScript.ShaderUsage = EditorGUILayout.Foldout(LoadBundleScript.ShaderUsage, "Shaders used in Asset", true);
 
             if (LoadBundleScript.ShaderUsage)
             {
@@ -33,18 +44,20 @@ namespace FACS01.Utilities
                     {
                         (ShaderUsageIn, MaterialUsageIn) = LoadBundleScript.getShaderUsage();
                     }
-                    
-                    EditorGUILayout.TextArea($"<color=cyan><b>{ShaderUsageIn.Count}</b> different shaders</color> were found:", newstyle);
-                    //scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+                    EditorGUILayout.LabelField($"<color=cyan><b>{ShaderUsageIn.Count}</b> different shaders</color> were found:", newstyle);
 
                     for (int i = 0; i < ShaderUsageIn.Count; i++)
                     {
                         string temp = String.Join("\n\t", MaterialUsageIn[i]);
-                        EditorGUILayout.TextArea($"<color=cyan><b>{ShaderUsageIn[i]}</b></color>\n\t{temp}", newstyle);
+                        EditorGUILayout.LabelField($"<color=cyan><b>{ShaderUsageIn[i]}</b></color>\n\t{temp}", newstyle);
                     }
-                    //EditorGUILayout.EndScrollView();
                 }
-                else GUILayout.TextArea("Load the Bundle on Play Mode!");
+                else EditorGUILayout.LabelField("Load the Bundle on Play Mode!", newstyle);
+            }
+            else if (ShaderUsageIn != null || MaterialUsageIn != null)
+            {
+                (ShaderUsageIn, MaterialUsageIn) = (null, null);
             }
         }
     }
