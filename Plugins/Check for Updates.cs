1f744a6075e6d1ffb8056e3b1285850c232ebd69
FACS01-01 <75183551+FACS01-01@users.noreply.github.com>
2022-04-29 20:47:13 -0400

    Update Check for Updates.cs
    option for stopping automatic update check

--- Plugins/Check for Updates.cs
+++ Plugins/Check for Updates.cs
@@ -16,18 +16,26 @@ namespace FACS01.Utilities
         public static SynchronizationContext syncContext = SynchronizationContext.Current;
         static CheckForUpdates()
         {
-            if (SessionState.GetBool("FACSUtilitiesCheckForUpdatesOnce", true))
+            if (SessionState.GetBool("FACSUtilities_CheckUpdatesStartup", true))
             {
-                SessionState.SetBool("FACSUtilitiesCheckForUpdatesOnce", false);
-                StartCFUThread();
+                SessionState.SetBool("FACSUtilities_CheckUpdatesStartup", false);
+                if (PlayerPrefs.GetInt("FACSUtilities_CheckUpdates", 1) == 1) StartCFUThread();
             }
         }
-        [MenuItem("FACS Utils/Misc/Check for Updates", false, 999)]
-        public static void StartCFUThread()
-        {   
+
+        [MenuItem("FACS Utils/Check for Updates", false, 999)]
+        public static void CheckForUpdatesManual()
+        {
+            StartCFUThread(true);
+        }
+
+        public static void StartCFUThread(bool manual = false)
+        {
+            if (manual) PlayerPrefs.SetInt("FACSUtilities_CheckUpdates", 1);
             Thread thread = new Thread(() => CheckForInternetConnection());
             thread.Start();
         }
+
         public static void CheckForInternetConnection()
         {
             bool goodtogo = false;
@@ -40,6 +48,7 @@ namespace FACS01.Utilities
             { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Can't Check for Updates. no Internet connection? or GitHub is down?"); }
             if (goodtogo) syncContext.Post(_ => { RunCFU(); }, null);
         }
+
         public static void RunCFU()
         {
             string myVersion = File.ReadLines(Application.dataPath + "/FACS01 Utilities/version.txt").First();
@@ -57,7 +66,7 @@ namespace FACS01.Utilities
             if (ver > latestver)
             { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Are you a time traveller? Tools version is higher than source!"); return; }
 
-            bool update;
+            int updateN;
             string lastFullReinstall = "";
             using (WebClient wc = new WebClient())
             { lastFullReinstall = wc.DownloadString("https://raw.githubusercontent.com/FACS01-01/FACS_Utilities/main/last%20full%20reinstall.txt"); }
@@ -67,17 +76,19 @@ namespace FACS01.Utilities
             if (ver < lastfullreins)
             {
                 Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date, and are incompatible with newer versions.");
-                update = EditorUtility.DisplayDialog("FACS Utilities : Check for Updates", "Your tools are out of date," +
+                updateN = EditorUtility.DisplayDialogComplex("FACS Utilities : Check for Updates", "Your tools are out of date," +
                     " and are incompatible with newer versions!\n\nGo to FACS Utilities GitHub page and get the latest Release.\nAnd remember to delete 'FACS Utilities' folder" +
-                    " before importing the newer one.", "Open GitHub", "Ignore");
-                if (update) Process.Start("https://github.com/FACS01-01/FACS_Utilities");
+                    " before importing the newer one!", "Open GitHub", "Ignore", "Don't Remind Again");
+                if (updateN == 0) Process.Start("https://github.com/FACS01-01/FACS_Utilities");
+                else if (updateN == 2) { PlayerPrefs.SetInt("FACSUtilities_CheckUpdates", 0); }
                 return;
             }
 
             Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date.");
-            update = EditorUtility.DisplayDialog("FACS Utilities : Check for Updates", "Your tools are out of date!\n\n" +
-                "Go to FACS Utilities GitHub page and get the latest Release.", "Open GitHub", "Ignore");
-            if (update) Process.Start("https://github.com/FACS01-01/FACS_Utilities");
+            updateN = EditorUtility.DisplayDialogComplex("FACS Utilities : Check for Updates", "Your tools are out of date!\n\n" +
+                "Go to FACS Utilities GitHub page and get the latest Release!", "Open GitHub", "Ignore", "Don't Remind Again");
+            if (updateN == 0) Process.Start("https://github.com/FACS01-01/FACS_Utilities");
+            else if (updateN == 2) { PlayerPrefs.SetInt("FACSUtilities_CheckUpdates", 0); }
         }
     }
 }
