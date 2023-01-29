d996c97558a6e33cd0693c0cebf1cbe75e768336
FACS01-01 <75183551+FACS01-01@users.noreply.github.com>
2023-01-29 17:33:27 -0300

    Update CheckForUpdates.cs
    cleanup

--- Plugins/CheckForUpdates.cs
+++ Plugins/CheckForUpdates.cs
@@ -39,13 +39,16 @@ namespace FACS01.Utilities
         public static void CheckForInternetConnection()
         {
             bool goodtogo = false;
-            try { using (var client = new WebClient())
+            try
+            {
+                using (var client = new WebClient())
                 {
                     using (client.OpenRead("https://raw.githubusercontent.com/FACS01-01/FACS_Utilities/main/version.txt"))
-                    {goodtogo = true;}
-            }}
+                    { goodtogo = true; }
+                }
+            }
             catch
-            { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Can't Check for Updates. no Internet connection? or GitHub is down?"); }
+            { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Can't Check for Updates. no Internet connection? or GitHub is down?\n"); }
             if (goodtogo) syncContext.Post(_ => { RunCFU(); }, null);
         }
 
@@ -61,10 +64,10 @@ namespace FACS01.Utilities
             decimal latestver = int.Parse(latestVersionSplit[0]) * 365.25m + int.Parse(latestVersionSplit[1]) * 30.4375m + int.Parse(latestVersionSplit[2]);
 
             if (ver == latestver)
-            { Debug.Log($"[<color=cyan>FACS Utilities</color>] Tools are up to date!"); return; }
+            { Debug.Log($"[<color=cyan>FACS Utilities</color>] Tools are up to date!\n"); return; }
 
             if (ver > latestver)
-            { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Are you a time traveller? Tools version is higher than source!"); return; }
+            { Debug.LogWarning($"[<color=cyan>FACS Utilities</color>] Are you a time traveller? Tools version is higher than source!\n"); return; }
 
             int updateN;
             string lastFullReinstall = "";
@@ -75,7 +78,7 @@ namespace FACS01.Utilities
 
             if (ver < lastfullreins)
             {
-                Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date, and are incompatible with newer versions.");
+                Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date, and are incompatible with newer versions.\n");
                 updateN = EditorUtility.DisplayDialogComplex("FACS Utilities : Check for Updates", "Your tools are out of date," +
                     " and are incompatible with newer versions!\n\nGo to FACS Utilities GitHub page and get the latest Release.\nAnd remember to delete 'FACS Utilities' folder" +
                     " before importing the newer one!", "Open GitHub", "Ignore", "Don't Remind Again");
@@ -84,7 +87,7 @@ namespace FACS01.Utilities
                 return;
             }
 
-            Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date.");
+            Debug.Log($"[<color=cyan>FACS Utilities</color>] Your tools are out of date.\n");
             updateN = EditorUtility.DisplayDialogComplex("FACS Utilities : Check for Updates", "Your tools are out of date!\n\n" +
                 "Go to FACS Utilities GitHub page and get the latest Release!", "Open GitHub", "Ignore", "Don't Remind Again");
             if (updateN == 0) Process.Start("https://github.com/FACS01-01/FACS_Utilities");
